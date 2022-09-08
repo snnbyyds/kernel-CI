3f4749146339cb9e300a0f9d39679586d93d571e
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2022-09-08 19:45:32 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,8 @@
 
 cd ~
 #git clone https://github.com/acuicultor/Radioactive_kernel_op6.git ~/linux
-git clone --depth=1 -b dot11 https://github.com/dotOS-Devices/android_kernel_oneplus_fajita.git ~/linux
+git clone  -b lineage-19.1  https://github.com/LineageOS/android_kernel_oneplus_sdm845.git ~/linux
+#git clone --depth=1 -b dot11 https://github.com/dotOS-Devices/android_kernel_oneplus_fajita.git ~/linux
 #git clone -b release_Q https://github.com/snnbyyds/Radioactive_kernel_op6.git ~/linux
 #git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 ~/aarch64-linux-android-4.9
 wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/961622e926a1b21382dba4dd9fe0e5fb3ee5ab7c.tar.gz
