721fec0171b3aad1383b37af7aa563fee2c3d50d
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2022-09-08 09:58:05 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,13 @@
 
 cd ~
 git clone https://github.com/acuicultor/Radioactive_kernel_op6.git ~/linux
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 ~/aarch64-linux-android-4.9
-export ANDROID_AARCH64=~/aarch64-linux-android-4.9/bin
+#git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 ~/aarch64-linux-android-4.9
+wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/961622e926a1b21382dba4dd9fe0e5fb3ee5ab7c.tar.gz
+mkdir toolchain
+cd toolchain
+tar -xpvf ../*.tar.gz && ls -l
+cd ~
+export ANDROID_AARCH64=~/toolchain/bin
 export ARCH=arm64
 export SUBARCH=arm64
 export PATH=$PATH:$ANDROID_AARCH64
