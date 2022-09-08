277f9552a8ee61163123ed0625e41fb162de202c
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2022-09-08 10:08:43 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd ~
-git clone https://github.com/acuicultor/Radioactive_kernel_op6.git ~/linux
+#git clone https://github.com/acuicultor/Radioactive_kernel_op6.git ~/linux
+git clone -b release_Q https://github.com/snnbyyds/Radioactive_kernel_op6.git ~/linux
 #git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 ~/aarch64-linux-android-4.9
 wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/961622e926a1b21382dba4dd9fe0e5fb3ee5ab7c.tar.gz
 mkdir toolchain
