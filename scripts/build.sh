7424b9064066751cf9d296ac16092b1a433a8b87
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2022-09-17 16:02:56 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 cd ~
 #git clone https://github.com/acuicultor/Radioactive_kernel_op6.git ~/linux
-git clone --depth=1 -b lineage-20.0 https://github.com/snnbyyds/android_kernel_oneplus_sdm845-2.git ~/linux
+git clone --depth=1 -b lineage-19.1 https://github.com/Linux-Mobile/android_kernel_oneplus_sdm845.git ~/linux
 #git clone --depth=1 -b snow https://github.com/snnbyyds/kernel_oneplus_sdm845.git ~/linux
 
 #wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/961622e926a1b21382dba4dd9fe0e5fb3ee5ab7c.tar.gz
