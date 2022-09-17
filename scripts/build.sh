5d2ac272e2fa278c040bb0ef36dd27fd4dfebd98
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2022-09-17 13:04:27 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,22 +2,24 @@
 
 cd ~
 #git clone https://github.com/acuicultor/Radioactive_kernel_op6.git ~/linux
-#git clone --depth=1 -b lineage-19.1  https://github.com/LineageOS/android_kernel_oneplus_sdm845.git ~/linux
-git clone --depth=1 -b 12 https://github.com/Havoc-fajita/a12kernel_oneplus_sdm845.git ~/linux
+git clone --depth=1 -b lineage-19.1 https://github.com/Linux-Mobile/android_kernel_oneplus_sdm845.git ~/linux
+#git clone --depth=1 -b 12 https://github.com/Havoc-fajita/a12kernel_oneplus_sdm845.git ~/linux
 #git clone -b release_Q https://github.com/snnbyyds/Radioactive_kernel_op6.git ~/linux
-#git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 ~/aarch64-linux-android-4.9
-wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/961622e926a1b21382dba4dd9fe0e5fb3ee5ab7c.tar.gz
+
+#wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/961622e926a1b21382dba4dd9fe0e5fb3ee5ab7c.tar.gz
+
 mkdir toolchain
+git clone https://github.com/radcolor/aarch64-elf.git toolchain
 cd toolchain
-tar -xpf ../*.tar.gz
+#tar -xpf ../*.tar.gz
 cd ~
 export ANDROID_AARCH64=~/toolchain/bin
 export ARCH=arm64
 export SUBARCH=arm64
 export PATH=$PATH:$ANDROID_AARCH64
-export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE=aarch64-elf-
 cd ~/linux
 mkdir output
-make O=output primus_defconfig -j8
+make O=output enchilada_defconfig -j8
 cd output
 make O=output -j8
